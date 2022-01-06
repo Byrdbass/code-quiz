674bc188f759fb3cae7fd52570fc7b8020371fbf
ByrdBassPC <byrdbass@gmail.com>
2022-01-06 15:58:46 -0600

    found a way to add score to each question and used 'this' to keep track of users selection

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@ var startButton = document.getElementById("startButton");
 var question = document.getElementById("question");
 var headingQuestion = document.getElementById("heading-question");
 var scorePage = document.getElementById("scorePage");
+var correctAnswer = '';
 // use the same class for every question from the HTML
 var button1 = document.getElementById("choice1");
 var button2 = document.getElementById("choice2");
@@ -22,46 +23,71 @@ var listOfQuestions = [
         choice1: "split",
         choice2: "Math.Floor", //this is the correct answer
         choice3: "math.random",
-        choice4: "slice"
+        choice4: "slice",
+        correctAnswer: "Math.Floor"
     },
     {
         heading: "Which of the following is a method?",
-        choice1: "split",
+        choice1: "split", //this is the correct answer
         choice2: "for",
         choice3: "if else",
-        choice4: "what else"
+        choice4: "what else",
+        correctAnswer: "split"
     },
     {
         heading: "Common Data Types in JavaScript DO NOT include...",
-        choice1: "alerts",
-        choice2: "booleans",
+        choice1: "alerts", //this is the correct answer
+        choice2: "booleans", 
         choice3: "numbers",
-        choice4: "strings"
+        choice4: "strings",
+        correctAnswer: "alerts"
     },
     {
         heading: "if else statements contained within",
-        choice1: "parenthesis",
-        choice2: "curly brackets",
+        choice1: "parenthesis", 
+        choice2: "curly brackets", //this is the correct answer
         choice3: "square brackets",
-        choice4: "quotes"
+        choice4: "quotes",
+        correctAnswer: "curly brackets"
     },
     {
         heading: "for loops must have what notation within the parenthesis",
-        choice1: "semicolon",
+        choice1: "semicolon", //this is the correct answer
         choice2: "commas",
         choice3: "quotes",
-        choice4: "slashes"
+        choice4: "slashes",
+        correctAnswer: "semicolon"
     }
 ];
 //need to create a variable for current Question
 var currentQuestion = listOfQuestions[questionIndex];
 
-// startPage dissapears after user clicks startButton
+// startPage disappears after user clicks startButton
 //advancePage1 runs after the startPage
 startButton.addEventListener("click", advancePage);
 question.style.display = "none";
 scorePage.style.display = "none";
-
+function checkAnswer() {
+    console.log("this is the correct answer ", listOfQuestions[questionIndex].correctAnswer);
+    console.log("this is what the user picked ", this);
+    console.log(this.textContent);
+    if (listOfQuestions[questionIndex].correctAnswer === this.textContent) {
+        score += 5;
+        console.log(score);
+    } else {
+        secondsLeft -= 2;
+    }
+    // currentQuestion = listOfQuestions[questionIndex += 1];
+    if (questionIndex === listOfQuestions.length -1) {
+        question.style.display = "none"
+        scorePage.style.display = "block";
+        finalScore.textContent = "Total points: " + score;
+        return;
+    } else {
+        currentQuestion = listOfQuestions[questionIndex += 1];
+        showQuestion();
+    }
+}
 function showQuestion() {
     currentQuestion.heading
     console.log(currentQuestion.heading);
@@ -71,32 +97,30 @@ function showQuestion() {
     button2.textContent = currentQuestion.choice2; 
     button3.textContent = currentQuestion.choice3;
     button4.textContent = currentQuestion.choice4;
-    var nextButton = document.getElementById("nextButton");
-    nextButton.textContent = "Next question";
-    currentQuestion = listOfQuestions[questionIndex += 1];
+    // var nextButton = document.getElementById("nextButton");
+    // nextButton.textContent = "Next question";
+    button1.addEventListener("click", checkAnswer);
+    button2.addEventListener("click", checkAnswer);
+    button3.addEventListener("click", checkAnswer);
+    button4.addEventListener("click", checkAnswer);
+    
     console.log(secondsLeft + " seconds left");
     console.log("you are on question #" + questionIndex)
-    if (secondsLeft === 0) {
-        question.style.display = "none"
-        scorePage.style.display = "block";
-        finalScore.textContent = "You ran out of time! Total points: " + score;
-    } else 
-    if (questionIndex === 5) {
-        question.style.display = "none"
-        scorePage.style.display = "block";
-        finalScore.textContent = "Total points: " + score;
-        return;
-    }
+
     };
-    function setTimer() {
-        var timerInterval = setInterval(function () {
-            secondsLeft --;
-            timeLeft.textContent = secondsLeft + " seconds left!"
-            if(secondsLeft === 0) {
-                clearInterval(timerInterval);
-            }        
-        }, 1000);
-    }
+function setTimer() {
+    var timerInterval = setInterval(function () {
+        secondsLeft --;
+        timeLeft.textContent = secondsLeft + " seconds left!"
+        if(secondsLeft === 0) {
+            clearInterval(timerInterval);
+            alert("You ran out of time!");
+            question.style.display = "none"
+            scorePage.style.display = "block";
+            finalScore.textContent = "Total points: " + score;
+        }        
+    }, 1000);
+}
 //question 1 appears and startPage is hidden
 function advancePage() {
     console.log("click")
@@ -118,7 +142,7 @@ console.log(secondsLeft);
 //two ways to do
 //use keyword of "this"
 //google javascript t
-nextButton.addEventListener("click", showQuestion);
+// nextButton.addEventListener("click", showQuestion);
 
 //I will need to create another function to advance to next question
 
